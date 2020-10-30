590437a701ae96a08466eaf40b79455068bd9c18
DavidRMorphew <morphewd@umich.edu>
2020-10-30 13:53:38 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each do {|key, value| self.send(("#{key}="), value) if value && value != []}
   end
 
   def self.create_from_collection(students_array)
